f9836b5e31a591c2afa00f4ce93f5177728b4151
Zhentao <shizhentao@gmail.com>
2019-12-01 00:18:02 +0800

    update before the video lecture

--- install.R
+++ install.R
@@ -1,11 +1,8 @@
-# install.packages("tidyverse")
-# install.packages("rmarkdown")
-# install.packages("httr")
-# install.packages("shinydashboard")
-# install.packages('leaflet')
+# specified the R packages needed for the IPython Notebook
 
 install.packages("quantmod")
 install.packages("Quandl")
 install.packages("dynlm")
 install.packages("plyr")
 install.packages("tsDyn")
+install.packages("mFilter")
